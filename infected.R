67406365f483d62b5bfefea3a3b0a769aee7fdfa
Konrad Klawonn <zeltbrennt@gmail.com>
2020-03-07 21:23:01 +0100

    added archive

--- infected.R
+++ infected.R
@@ -10,12 +10,12 @@ temp <- read_html("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/
 new_row <- temp[[1]][,1:2] %>%
   pivot_wider(names_from = Bundesland, values_from = `Fälle`) %>%
   mutate(Stand = Sys.Date())
-old_data <- read_csv(paste0(Sys.Date()-1, "-infizierte.csv"))
+old_data <- read_csv(paste0("archive/", Sys.Date()-1, "-infizierte.csv"))
 
 old_data %>%
   bind_rows(new_row) %>%
   write_csv(paste0(Sys.Date(), "-infizierte.csv"))
 
 # todo:
-# - old files into archive folder
 # - create plots
+# - automate git update
--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+for f in *infizierte.csv; do
+  mv "$f" "archive/$f"
+done
+
 Rscript infected.R
-git add -A
-git commit -m "update data"
+#git add -A
+#git commit -m "update data"
