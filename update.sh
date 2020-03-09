03852f7e0dbbd0c420a62017a825e09fa187e7bf
zeltbrennt <zeltbrennt@gmail.com>
2020-03-09 19:17:39 +0000

    made cronjob work

--- update.sh
+++ update.sh
@@ -6,5 +6,5 @@ Rscript /home/pi/corona/infected.R
 
 cd /home/pi/corona && /usr/bin/git add -A
 cd /home/pi/corona && /usr/bin/git commit -m "update data"
-cd /home/pi/corona && /usr/bin/git push origin dev
+cd /home/pi/corona && /usr/bin/git push origin master
 
