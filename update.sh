de955c7be18336c67eb8468aadb5d6418ff75a33
zeltbrennt <zeltbrennt@gmail.com>
2020-03-11 07:05:30 +0100

    fortified upload strategy

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cd /home/pi/corona
+/usr/bin/git checkout master
+/usr/bin/git pull origin master
+
 /usr/bin/Rscript /home/pi/corona/infected.R
 
-cd /home/pi/corona
 /usr/bin/git add -A
-/usr/bin/git commit -m "update data"
+/usr/bin/git commit -m "auto update"
 /usr/bin/git push origin master
